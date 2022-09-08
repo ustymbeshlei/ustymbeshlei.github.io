2b62a9be3b3846db50b72a379a81f3a44f4c3ea5
Ustym <ystumys@ukr.net>
2022-09-08 23:21:51 +0300

    added main page slmagro

--- SLMAGRO/js/script.js
+++ SLMAGRO/js/script.js
@@ -36,31 +36,66 @@ window.addEventListener("DOMContentLoaded", () => {
     window.addEventListener("scroll", navHighlighter);
 
     function navHighlighter() {
-  
+
         // Get current scroll position
         let scrollY = window.pageYOffset;
-        
+
         // Now we loop through sections to get height, top and ID values for each
         sections.forEach(current => {
-          const sectionHeight = current.offsetHeight;
-          const sectionTop = current.offsetTop - 50;
-          const sectionId = current.getAttribute("id");
-          
-          /*
-          - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
-          - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
-          */
-          if (
-            scrollY > sectionTop &&
-            scrollY <= sectionTop + sectionHeight
-          ){
-            document.querySelector(".menu a[href*=" + sectionId + "]").classList.add("active");
-          } else {
-            document.querySelector(".menu a[href*=" + sectionId + "]").classList.remove("active");
-          }
-        });
-      }
+            const sectionHeight = current.offsetHeight;
+            const sectionTop = current.offsetTop - 50;
+            const sectionId = current.getAttribute("id");
 
-      new WOW().init();
+            /*
+            - If our current scroll position enters the space where current section on screen is, add .active class to corresponding navigation link, else remove it
+            - To know which link needs an active class, we use sectionId variable we are getting while looping through sections as an selector
+            */
+            if (
+                scrollY > sectionTop && scrollY <= sectionTop + sectionHeight
+            ) {
+                document.querySelector(".menu a[href*=" + sectionId + "]").classList.add("active");
+            } else {
+                document.querySelector(".menu a[href*=" + sectionId + "]").classList.remove("active");
+            }
+        });
+    }
+
+    new WOW().init();
+
+
+    // const filterButtons = document.querySelectorAll('.filter-main-products__btn'),
+    //       productsItem = document.querySelectorAll('.products__item');
+
+
+    //       filterButtons.forEach((e) => {
+    //         e.addEventListener('click', () => {
+    //             if (!e.classList.contains('filter-main-products__btn_active')) {
+    //                 e.classList.add('filter-main-products__btn_active');
+    //                 // hamburgerCheckbox.checked = false;
+    //             }
+
+    //             // e.classList.add('filter-main-products__item_active')
+    
+    //         })
+    //     })
+
+    // function filterSelection()
+
+    $('ul.filter-main-products__list').on('click', 'li:not(.filter-main-products__item_active)', function () {
+        $(this)
+            .addClass('filter-main-products__item_active').siblings().removeClass('filter-main-products__item_active').closest('div.container').find('ul.main-products__list').removeClass('main-products__list_active').eq($(this).index()).addClass('main-products__list_active');
+    });
 });
+
+// (function ($) {
+//     $(function () {
+
+//     $('ul.filter-main-products__list').on('click', 'li:not(.filter-main-products__item_active)', function () {
+//         $(this)
+//             .addClass('filter-main-products__item_active').siblings().removeClass('filter-main-products__item_active')
+//             .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+//     });
+
+//     });
+// })(jQuery);
 
